013ac9763558b32f640dcfc3c95f8cfd6176065b
AlexaD09 <dayaproyectos12@gmail.com>
2026-01-18 17:57:59 -0500

    Test infrastructure min

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -1,15 +1,29 @@
 variable "env" {
-  default = "qa"
+  description = "Environment (qa or prod)"
+  type        = string
+  default     = "qa"
 }
 
 variable "region" {
-  default = "us-east-1"
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
 }
 
 variable "ami_id" {
-  default = "ami-0c02fb55956c7d316"
+  description = "Amazon Linux 2 AMI"
+  type        = string
+  default     = "ami-0c02fb55956c7d316"
 }
 
 variable "instance_type" {
-  default = "t3.micro"
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "admin_cidr" {
+  description = "Admin IP for Bastion access"
+  type        = string
+  default     = "0.0.0.0/0"
 }
