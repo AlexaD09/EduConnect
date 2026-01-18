6b7f8b421f32f8604300242c56db202615c16e7c
AlexaD09 <dayaproyectos12@gmail.com>
2026-01-18 10:11:14 -0500

    CI/CD and Turborepo implementation

--- infra/terraform/vcp.tf
+++ infra/terraform/vcp.tf
@@ -1,16 +1,4 @@
-# VPC QA
-resource "aws_vpc" "qa_frontend" {
-  provider   = aws.frontend
-  cidr_block = "10.0.0.0/16"
-  tags = { Name = "${var.env}-frontend-vpc" }
-}
-
-resource "aws_vpc" "qa_ms_a" {
-  provider   = aws.ms_a
-  cidr_block = "10.0.0.0/16"
-  tags = { Name = "${var.env}-ms-a-vpc" }
-}
-# VPCs QA (nombres fijos)
+# VPCs QA
 resource "aws_vpc" "qa_frontend" {
   provider   = aws.frontend
   cidr_block = "10.0.0.0/16"
@@ -41,7 +29,7 @@ resource "aws_vpc" "qa_bastion" {
   tags = { Name = "qa-bastion-vpc" }
 }
 
-# VPCs PROD 
+# VPCs PROD
 resource "aws_vpc" "prod_frontend" {
   provider   = aws.frontend
   cidr_block = "10.1.0.0/16"
@@ -70,52 +58,4 @@ resource "aws_vpc" "prod_bastion" {
   provider   = aws.bastion
   cidr_block = "10.1.0.0/16"
   tags = { Name = "prod-bastion-vpc" }
-}
-resource "aws_vpc" "qa_ms_b" {
-  provider   = aws.ms_b
-  cidr_block = "10.0.0.0/16"
-  tags = { Name = "${var.env}-ms-b-vpc" }
-}
-
-resource "aws_vpc" "qa_databases" {
-  provider   = aws.databases
-  cidr_block = "10.0.0.0/16"
-  tags = { Name = "${var.env}-databases-vpc" }
-}
-
-resource "aws_vpc" "qa_bastion" {
-  provider   = aws.bastion
-  cidr_block = "10.0.0.0/16"
-  tags = { Name = "${var.env}-bastion-vpc" }
-}
-
-# VPC PROD
-resource "aws_vpc" "prod_frontend" {
-  provider   = aws.frontend
-  cidr_block = "10.1.0.0/16"
-  tags = { Name = "${var.env}-frontend-vpc" }
-}
-
-resource "aws_vpc" "prod_ms_a" {
-  provider   = aws.ms_a
-  cidr_block = "10.1.0.0/16"
-  tags = { Name = "${var.env}-ms-a-vpc" }
-}
-
-resource "aws_vpc" "prod_ms_b" {
-  provider   = aws.ms_b
-  cidr_block = "10.1.0.0/16"
-  tags = { Name = "${var.env}-ms-b-vpc" }
-}
-
-resource "aws_vpc" "prod_databases" {
-  provider   = aws.databases
-  cidr_block = "10.1.0.0/16"
-  tags = { Name = "${var.env}-databases-vpc" }
-}
-
-resource "aws_vpc" "prod_bastion" {
-  provider   = aws.bastion
-  cidr_block = "10.1.0.0/16"
-  tags = { Name = "${var.env}-bastion-vpc" }
 }
