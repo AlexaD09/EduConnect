b303ce0f0dcc0506adf9377c76f14839f8234731
AlexaD09 <dayaproyectos12@gmail.com>
2026-01-27 16:30:00 -0500

    new deploy

--- infra/terraform/kepairs.tf
+++ infra/terraform/kepairs.tf
@@ -3,8 +3,9 @@ resource "tls_private_key" "ssh" {
   algorithm = "RSA"
   rsa_bits  = 2048
 }
+
 resource "aws_key_pair" "shared" {
-  key_name   = "prueba-${var.environment}"
+  key_name   = "prueba-${var.environment}-v2"
   public_key = tls_private_key.ssh.public_key_openssh
 }
 
