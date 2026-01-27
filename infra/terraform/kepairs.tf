dc529fae99bf65f6f2e8d35885009e47139097cc
AlexaD09 <dayaproyectos12@gmail.com>
2026-01-27 15:33:00 -0500

    new deploy

--- infra/terraform/kepairs.tf
+++ infra/terraform/kepairs.tf
@@ -5,32 +5,7 @@ resource "tls_private_key" "ssh" {
 }
 
 
-resource "aws_key_pair" "frontend" {
-  provider   = aws.frontend
-  key_name   = "${var.key_name}-${var.environment}"
-  public_key = tls_private_key.ssh.public_key_openssh
-}
-
-resource "aws_key_pair" "microservices_a" {
-  provider   = aws.microservices_a
-  key_name   = "${var.key_name}-${var.environment}"
-  public_key = tls_private_key.ssh.public_key_openssh
-}
-
-resource "aws_key_pair" "microservices_b" {
-  provider   = aws.microservices_b
-  key_name   = "${var.key_name}-${var.environment}"
-  public_key = tls_private_key.ssh.public_key_openssh
-}
-
-resource "aws_key_pair" "data" {
-  provider   = aws.data
-  key_name   = "${var.key_name}-${var.environment}"
-  public_key = tls_private_key.ssh.public_key_openssh
-}
-
-resource "aws_key_pair" "bastion" {
-  provider   = aws.bastion
-  key_name   = "${var.key_name}-${var.environment}"
-  public_key = tls_private_key.ssh.public_key_openssh
+resource "aws_key_pair" "shared" {
+  key_name   = "prueba-${var.environment}"
+  public_key = file(var.public_key_path)
 }
