375ebdc7b62a8c3438079d32650f5c3968518a6b
AlexaD09 <dayaproyectos12@gmail.com>
2026-01-31 07:51:09 -0500

    ci/cd: turbo build+push and deploy only changed services; expose service_endpoints from terraform

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -45,3 +45,30 @@ output "mongo_private_ip"    { value = module.data_mongo.private_ips[0] }
 output "rabbit_private_ip"   { value = module.data_rabbitmq.private_ips[0] }
 output "mqtt_private_ip"     { value = module.data_mqtt.private_ips[0] }
 output "n8n_private_ip"      { value = module.data_n8n.private_ips[0] }
+
+output "service_endpoints" {
+  value = {
+    "frontend-web"        = { ip = module.frontend_web.private_ips[0],            port = 80 }
+    "api-gateway"         = { ip = module.ms_api_gateway.private_ips[0],          port = 8000 }
+
+    "activity-service"    = { ip = module.ms_activity_service.private_ips[0],     port = 8000 }
+    "agreement-service"   = { ip = module.ms_agreement_service.private_ips[0],    port = 8000 }
+    "approval-service"    = { ip = module.ms_approval_service.private_ips[0],     port = 8000 }
+    "audit-service"       = { ip = module.ms_audit_service.private_ips[0],        port = 8000 }
+    "user-service"        = { ip = module.ms_user_service.private_ips[0],         port = 8000 }
+
+    "backup-service"      = { ip = module.ms_backup_service.private_ips[0],       port = 8000 }
+    "document-service"    = { ip = module.ms_document_service.private_ips[0],     port = 8000 }
+    "event-service"       = { ip = module.ms_event_service.private_ips[0],        port = 8000 }
+    "evidence-service"    = { ip = module.ms_evidence_service.private_ips[0],     port = 8000 }
+    "notification-service"= { ip = module.ms_notification_service.private_ips[0], port = 8000 }
+
+    "postgres"            = { ip = module.data_postgres.private_ips[0],           port = 5432 }
+    "redis"               = { ip = module.data_redis.private_ips[0],              port = 6379 }
+    "kafka"               = { ip = module.data_kafka.private_ips[0],              port = 9092 }
+    "mongo"               = { ip = module.data_mongo.private_ips[0],              port = 27017 }
+    "rabbitmq"            = { ip = module.data_rabbitmq.private_ips[0],           port = 5672 }
+    "mqtt"                = { ip = module.data_mqtt.private_ips[0],               port = 1883 }
+    "n8n"                 = { ip = module.data_n8n.private_ips[0],                port = 5678 }
+  }
+}
